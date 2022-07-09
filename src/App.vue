a15c4455088d6ab3891b0344ddd240db82d20d90
Alexis Mora <alexis.mora.sanchez@gmail.com>
2022-07-09 16:17:06 +0200

    wip: layout and header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
+import { defineAsyncComponent } from "vue"
 import SplashScreen from "./components/SplashScreen.vue"
+
+const BaseHome = defineAsyncComponent(()=>
+  import('@/components/BaseHome.vue')
+)
 </script>
 
 <template>
-  <SplashScreen />
+  <Suspense>
+    <template #default>
+      <BaseHome />
+    </template>
+    <template #fallback>
+      <SplashScreen />
+    </template>
+  </Suspense>
 </template>
 
 <style>
---
+++ src/components/BaseHeader.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="header">
+    <img
+      src="@/assets/logo.svg"
+      alt="pfa_logo"
+    >
+  </div>
+</template>
+
